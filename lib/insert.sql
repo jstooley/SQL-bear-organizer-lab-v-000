44986882a9c0cf34a54c60ada2581ce16ac62c70
Justin Tooley <jstooley823@gmail.com>
2017-09-26 03:43:23 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,14 +1,14 @@
 INSERT INTO bears (name, age, gender,color, temperament, alive) VALUES
-("Mr. Chocolate", 4, );
+("Mr. Chocolate", 4, 'M', 'Dark Brown', "Smooth", true );
 INSERT INTO bears (name, age, gender,color, temperament, alive) VALUES
-("Rowdy");
+("Rowdy", 1, 'M', 'Brown', 'Aggressive', false);
 INSERT INTO bears (name, age, gender,color, temperament, alive) VALUES
-("Tabitha");
+("Tabitha", 10, 'F', "Black", 'Slow', true);
 INSERT INTO bears (name, age, gender,color, temperament, alive) VALUES
-("Sergeant Brown");
+("Sergeant Brown", 11, 'M', 'Brown', "Aggressive", true);
 INSERT INTO bears (name, age, gender,color, temperament, alive) VALUES
-("Melissa");
+("Melissa", 7, "F", "Brown", "Smooth", false);
 INSERT INTO bears (name, age, gender,color, temperament, alive) VALUES
-("Grinch");
+("Grinch", 9, 'F', 'Black', "Mad", true);
 INSERT INTO bears (name, age, gender,color, temperament, alive) VALUES
-("Wendy");
+("Wendy",560, 'F', "Grey", "Old", true);
