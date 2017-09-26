fa7bae4d64fcf0390092f7aadf1a0c615e8f5ee5
Justin Tooley <jstooley823@gmail.com>
2017-09-26 03:45:53 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
 INSERT INTO bears (name, age, gender,color, temperament, alive) VALUES
-("Mr. Chocolate", 4, 'M', 'Dark Brown', "Smooth", true );
+("Mr. Chocolate", 4, 'M', 'Dark Brown', "Smooth", 1 );
 INSERT INTO bears (name, age, gender,color, temperament, alive) VALUES
-("Rowdy", 1, 'M', 'Brown', 'Aggressive', false);
+("Rowdy", 1, 'M', 'Brown', 'Aggressive', 0);
 INSERT INTO bears (name, age, gender,color, temperament, alive) VALUES
-("Tabitha", 10, 'F', "Black", 'Slow', true);
+("Tabitha", 10, 'F', "Black", 'Slow', 1);
 INSERT INTO bears (name, age, gender,color, temperament, alive) VALUES
-("Sergeant Brown", 11, 'M', 'Brown', "Aggressive", true);
+("Sergeant Brown", 11, 'M', 'Brown', "Aggressive", 1);
 INSERT INTO bears (name, age, gender,color, temperament, alive) VALUES
-("Melissa", 7, "F", "Brown", "Smooth", false);
+("Melissa", 7, "F", "Brown", "Smooth", 0);
 INSERT INTO bears (name, age, gender,color, temperament, alive) VALUES
-("Grinch", 9, 'F', 'Black', "Mad", true);
+("Grinch", 9, 'F', 'Black', "Mad", 1);
 INSERT INTO bears (name, age, gender,color, temperament, alive) VALUES
-("Wendy",560, 'F', "Grey", "Old", true);
+("Wendy",560, 'F', "Grey", "Old", 1);
 INSERT INTO bears (name, age, gender,color, temperament, alive) VALUES
-(Null ,0 , 'M', "Rainbow", "Colorful", true);
+(Null ,0 , 'M', "Rainbow", "Colorful", 1);
